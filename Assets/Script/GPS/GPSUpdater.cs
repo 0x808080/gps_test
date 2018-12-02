8636c1befc38ee3aad26368e569eabb9ecb8d4d4
falcon <falcon@dummy.com>
2018-12-03 00:48:41 +0900

    GPSベースプロジェクトをコミット

---
+++ Assets/Script/GPS/GPSUpdater.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSUpdater : MonoBehaviour {
+
+    public float IntervalSeconds = 1.0f;
+    public LocationServiceStatus Status;
+    public LocationInfo Location;
+    private bool gIsDialog = false;
+
+    /// =======================================================================
+    /// 起動スクリプト
+    /// =======================================================================
+    IEnumerator Start()
+    {
+        // 使う前に setlabel を呼んどく。
+        DialogManager.Instance.SetLabel("OK", "No", "Close");
+
+        while (true)
+        {
+            this.Status = Input.location.status;
+
+            if (Input.location.isEnabledByUser)
+            {
+                switch (this.Status)
+                {
+                    case LocationServiceStatus.Stopped:
+                        Input.location.Start();
+                        break;
+
+                    case LocationServiceStatus.Running:
+                        this.Location = Input.location.lastData;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                // 位置情報を有効にするようにユーザーへ要求
+                ShowDialog_gps();
+                gIsDialog = true;
+            }
+
+            // 指定した秒数後に再度判定を走らせる
+            yield return new WaitForSeconds(IntervalSeconds);
+        }
+    }
+
+
+    private void ShowDialog_gps()
+    {
+        if( gIsDialog)
+        {
+            return;
+        }
+
+        // YES NO ダイアログ
+        DialogManager.Instance.ShowSelectDialog(
+           "設定からGPSの使用を許可してください。",
+            (bool result) =>
+            {
+                gIsDialog = false;
+                Application.Quit();
+            });
+    }
+
+}
---
+++ Assets/Script/GPS/LocationRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationRenderer : MonoBehaviour {
+
+    public GPSUpdater updater;
+    public Text text;
+
+    void Update()
+    {
+        if(updater == null)
+        {
+            return;
+        }
+
+        text.text = updater.Status.ToString()
+                  + "\n" + "lat:" + updater.Location.latitude.ToString()
+                  + "\n" + "lng:" + updater.Location.longitude.ToString();
+    }
+}
